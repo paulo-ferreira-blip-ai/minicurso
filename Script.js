dda6e6858e6dc35c63f161006f670158aee6dec3
pauloren7 <paulorenan1515@gmail.com>
2022-01-14 20:22:54 -0300

    Adicionar estilo para topicos

---
+++ Script.js
@@ -0,0 +1,40 @@
+const imagem = document.querySelector('img');
+const botao = document.querySelector('button');
+const nomeDoPersonagem = document.querySelector('#nome');
+const especie = document.querySelector('#especie');
+const condicao = document.querySelector('#status');
+
+traduzirCondicao = (data) => {
+    if(data.status == 'unknow'){
+        return 'Não sabemos';
+    }else if(data.status == 'Alive'){
+        return 'Sim;'
+    }else {
+        return 'Não. Está morto';
+    }
+}
+
+gerarValorAleatorio = () => {
+    return Math.floor(Math.random() * 671);
+}
+
+pegarPersonagem = () => {
+    let numeroAleatorio = gerarValorAleatorio();
+    return fetch(`https://rickandmortyapi.com/api/character${numeroAleatorio}`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.alt = data.name;
+        nomeDoPersonagem.innerHTML = data.name;
+        especie.innerHTML = data.species;
+        condicao.innerHTML = data.status;
+        condicao.innerHTML = traduzirCondicao(data);
+
+    });
+}
+
+botao.onclick = pegarPersonagem;
